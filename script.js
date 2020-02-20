fdf7a7fbf2df9e7d5889d57cb90c21287a685eef
Somreth Peng <somreth916@gmail.com>
2020-02-20 07:01:38 -0800

    updated api

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 
 api = "9cbc884067adbaef3afbacaa15e0e389"
+city = "sacramento"
 
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=sacramento&units=imperial&appid=" + api, function (data) {
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + api, function (data) {
     console.log(data);
 
     var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
     var temp = data.main.temp;
     var humidity = data.main.humidity;
     var wind = data.wind.speed;
-    var uvIndex =
 
-
-        $(".icon").attr("src", icon);
+    $(".icon").attr("src", icon);
     $("#temp").append(" " + temp + " °F");
     $("#humidity").append(" " + humidity + " %");
     $("#wind").append(" " + wind + " MPH");
+
+    var uvIndex = "http://api.openweathermap.org/data/2.5/uvi?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=" + api;
+    var uvIndex1 = data.index;
+    $("#uvIndex").append(" " + uvIndex);
 });
