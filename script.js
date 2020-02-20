6ff60f481da50661dc9c5a2053d238e341e16811
Somreth Peng <somreth916@gmail.com>
2020-02-20 12:54:33 -0800

    updated api

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 api = "9cbc884067adbaef3afbacaa15e0e389"
 city = "sacramento"
 
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + api, function (data) {
+$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + api, function (data) {
     console.log(data);
 
-    var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
     var temp = data.main.temp;
     var humidity = data.main.humidity;
     var wind = data.wind.speed;
@@ -15,7 +15,7 @@ $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=i
     $("#humidity").append(" " + humidity + " %");
     $("#wind").append(" " + wind + " MPH");
 
-    var uvIndex = "http://api.openweathermap.org/data/2.5/uvi?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=" + api;
+    var uvIndex = "https://api.openweathermap.org/data/2.5/uvi?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=" + api;
     var uvIndex1 = data.index;
     $("#uvIndex").append(" " + uvIndex);
 });
