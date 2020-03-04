6cbebbf4e9c7fe490992e26a5ef0b4386b600e6e
Somreth Peng <somreth916@gmail.com>
2020-03-04 02:51:40 -0800

    updated 5 day forecast

--- script.js
+++ script.js
@@ -25,4 +25,50 @@ $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=
 
 $.getJSON("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + api, function (data) {
     console.log(data);
+    var date1 = data.list[0].dt_txt;
+    var icon1 = "https://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png";
+    var temp1 = data.list[0].main.temp;
+    var humid1 = data.list[0].main.humidity;
+
+    var date2 = data.list[4].dt_txt;
+    var icon2 = data.list[4]
+    var temp2 = data.list[4].main.temp;
+    var humid2 = data.list[4].main.humidity;
+
+    var date3 = data.list[12].dt_txt;
+    var icon3 = data.list[12]
+    var temp3 = data.list[12].main.temp;
+    var humid3 = data.list[12].main.humidity;
+
+    var date4 = data.list[20].dt_txt;
+    var icon4 = data.list[20]
+    var temp4 = data.list[20].main.temp;
+    var humid4 = data.list[20].main.humidity;
+
+    var date5 = data.list[28].dt_txt;
+    var icon5 = data.list[28]
+    var temp5 = data.list[28].main.temp;
+    var humid5 = data.list[28].main.humidity;
+
+    $("#date1").append(date1);
+    $("#temp1").append(" " + temp1 + " °F");
+    $("#humidity1").append(" " + humid1 + " %");
+
+    $("#date2").append(date2);
+    $("#temp2").append(" " + temp2 + " °F");
+    $("#humidity2").append(" " + humid2 + " %");
+
+    $("#date3").append(date3);
+    $("#temp3").append(" " + temp3 + " °F");
+    $("#humidity3").append(" " + humid3 + " %");
+
+    $("#date4").append(date4);
+    $("#temp4").append(" " + temp4 + " °F");
+    $("#humidity4").append(" " + humid4 + " %");
+
+    $("#date5").append(date5);
+    $("#temp5").append(" " + temp5 + " °F");
+    $("#humidity5").append(" " + humid5 + " %");
+
+
 });
